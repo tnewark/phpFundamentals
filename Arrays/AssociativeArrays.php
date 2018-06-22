b739823fe8bb5f7bfa9f2dfd1c7f62f4d5bd1652
Tom Newark <thomas.newark@phmc.com>
2018-06-22 16:22:19 -0500

    Added Associative Array demo

---
+++ Arrays/AssociativeArrays.php
@@ -0,0 +1,11 @@
+<?php
+
+$authors = array(
+        "quarky" => "Charles Dickens", 
+        10 => "Jane Austin", 
+        "poetic" => "William Shakespeare",
+        "Mark Twain"
+);
+
+print_r($authors);
+
